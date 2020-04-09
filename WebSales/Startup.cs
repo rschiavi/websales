1d7c630d7b71a5c2b3a266ab3209ca4d6eb3431a
rschiavi <mrc_rafa@hotmail.com>
2020-04-08 23:33:11 -0300

    seeding service

--- WebSales/Startup.cs
+++ WebSales/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using WebSales.Models;
+using WebSales.Data;
 
 namespace WebSales
 {
@@ -39,14 +40,17 @@ namespace WebSales
             services.AddDbContext<WebSalesContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("WebSalesContext"), builder =>
                         builder.MigrationsAssembly("WebSales")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
