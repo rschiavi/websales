799922fcd5fbe92a45420dd806b092e932b15e92
rschiavi <mrc_rafa@hotmail.com>
2020-04-09 00:03:22 -0300

    seller controller with Index and seller service

---
+++ WebSales/Services/SellerService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSales.Models;
+
+namespace WebSales.Services
+{
+    public class SellerService
+    {
+        private readonly WebSalesContext _context;
+
+        public SellerService(WebSalesContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
--- WebSales/Startup.cs
+++ WebSales/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using WebSales.Models;
 using WebSales.Data;
+using WebSales.Services;
 
 namespace WebSales
 {
@@ -42,6 +43,7 @@ namespace WebSales
                         builder.MigrationsAssembly("WebSales")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
