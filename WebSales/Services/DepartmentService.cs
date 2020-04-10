58fbe0114c9a525f3643aca3f0348e536e8701f9
rschiavi <mrc_rafa@hotmail.com>
2020-04-10 03:13:36 -0300

    SellerFormViewModel e select of departments in seller create

---
+++ WebSales/Services/DepartmentService.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using WebSales.Models;
+
+namespace WebSales.Services
+{
+    public class DepartmentService
+    {
+        private readonly WebSalesContext _context;
+
+        public DepartmentService(WebSalesContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(dep => dep.Name).ToList();
+        }
+    }
+}
