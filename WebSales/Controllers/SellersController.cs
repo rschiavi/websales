31a9c1e44d13d72cf84399f4dea4143ee7f4d0be
rschiavi <mrc_rafa@hotmail.com>
2020-04-10 22:11:40 -0300

    Seller details and eager loading

--- WebSales/Controllers/SellersController.cs
+++ WebSales/Controllers/SellersController.cs
@@ -41,6 +41,22 @@ namespace WebSales.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
         public IActionResult Delete(int? id)
         {
             if (id == null)
